00bfad0112adb4864746a4d212892f075f46aa0f
Your Name <phonglythanh14@gmail.com>
2021-12-18 21:05:04 +0700

    Update gitignore.

--- contracts/AkatherReward.sol
+++ contracts/AkatherReward.sol
@@ -5,7 +5,7 @@ import "./AkatherToken.sol";
 contract AkatherReward{
     IERC20 public token;
     uint256 numberOfContests;
-    enum ContestStatus{ NOTCREATED,CREATED, FINISHED, CLOSED}
+    enum ContestStatus{ NEW, FINISHED, CLOSED}
     struct Contest{
         uint256 id;
         address creator;
@@ -13,32 +13,42 @@ contract AkatherReward{
         string ContestName;
         uint256 prizeAmount;
         address winner;
+        uint256 beginTime;
+        uint256 endTime;
     }
     mapping(uint256 => Contest) public contests;
-    event ContestStatusChanged(uint256 id);
+    event ContestCreated(uint256 id);
+    event ContestFinished(uint256 id);
+    event ContestClosed(uint256 id);
     modifier onlyCreator(address _address,uint256 _contestId){
         require(contests[_contestId].creator == _address);
+        _;
+    }
+    modifier beganOnly(uint256 _time,uint256 _contestId){
+        require(_time >= contests[_contestId].beginTime);
         _;
     }
     constructor(address _tokenAddress){
         token = ERC20(_tokenAddress);   
     }
-    function createContest(string memory _contestName, uint _prizeAmount) public {
+    function createContest(string memory _contestName, uint _prizeAmount,uint256 beginTime) public {
         token.transferFrom(msg.sender, address(this), _prizeAmount);
         numberOfContests++;
-        contests[numberOfContests] = Contest(numberOfContests,msg.sender,ContestStatus.CREATED,_contestName,_prizeAmount,address(0)) ;
-        emit ContestStatusChanged(numberOfContests);
+        contests[numberOfContests] = Contest(numberOfContests,msg.sender,ContestStatus.NEW,_contestName,_prizeAmount,address(0),beginTime,0) ;
+        emit ContestCreated(numberOfContests);
     }
-    function finishContest(uint256 _contestId,address _winner) public onlyCreator(msg.sender,_contestId){
-        require(contests[_contestId].status == ContestStatus.CREATED);
+    function finishContest(uint256 _contestId,address _winner) public onlyCreator(msg.sender,_contestId) beganOnly(block.timestamp,_contestId){
+        require(contests[_contestId].status == ContestStatus.NEW);
         contests[_contestId].status = ContestStatus.FINISHED;
         contests[_contestId].winner = _winner;
+        contests[_contestId].endTime = block.timestamp;
+        emit ContestFinished(numberOfContests);
     }
     function withdrawRewards(uint256 _contestId) public{
-        require(contests[_contestId].status == ContestStatus.FINISHED);
+        require(contests[_contestId].status == ContestStatus.FINISHED,"The contest must be finished to be ");
         require(msg.sender == contests[_contestId].winner);
         contests[_contestId].status = ContestStatus.CLOSED;
         token.transfer(msg.sender, contests[_contestId].prizeAmount);
-        emit ContestStatusChanged(_contestId);
+        emit ContestClosed(_contestId);
     }
 }
